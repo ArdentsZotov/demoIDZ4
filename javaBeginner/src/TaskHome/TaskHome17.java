b54fa9780a6f158c6cdfe10f19d8ed99f065c111
QALance <chernikov.a@mail.ru>
2021-04-13 00:37:48 +0300

    Домашнее задание

--- javaBeginner/src/TaskHome/TaskHome17.java
+++ javaBeginner/src/TaskHome/TaskHome17.java
@@ -1,4 +1,3 @@
-package TaskHome;
 /*
 Итоговая задача №3 (Задание №17)
 Напишите программу, где пользователь вводит сначала количество строк n,
@@ -12,28 +11,64 @@ package TaskHome;
 Ответ: привет
 */
 
-import java.awt.*;
+//Вариант 1
+
+package TaskHome;
 import java.util.Scanner;
 public class TaskHome17 {
-    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+    /*
+        int count = 0;// объявил счетчик и обнулил его
+
+        Scanner in = new Scanner(System.in); // ввожу кол-во строк
         System.out.println("Введите количество строк: ");
         int n = in.nextInt();
-        String phrases[] = new String[n];
-        System.out.println("Количество строк: " + n);
-        for (int i = 0; i < n; i++) {
-            System.out.println("Введите строку " + (i + 1) + ": ");
-            phrases[i] = in.next();
-        }
-        for (int i = 0; i < phrases.length; i++) {
-            for (int j = 0; j < phrases.length; j++) {
-                if (phrases[i].length() > phrases[j].length()) {
-                    System.out.println(phrases[i]);
+
+        for (int i = 1; i <= n; i++) { // запускаю цикл очередности ввода строк
+            System.out.println("Введите строку " + i + ": ");
+            String phrases = in.next(); // ввожу строку*
+            //in.next() вводит только одно слово до пробела
+            //если поменять на in.nextLine(), чтобы вводить всю строку целиком,
+            //то программа завершится после выбора кол-ва строк
+            char array[] = phrases.toCharArray();// объявляю массив и добавляю в его элементы символы введенной строки
+            for (int j = 0; j < array.length - 1; j++) {// пробегаю по элементам массива
+                if (array[j] != array[j + 1] & array[array.length - 1] != array[j]) {
+                    count++;// если левый элемент != правому, то счетчик +1 - уникальный символ
                 }
-        }
-
             }
+            int maxStr = 0; // из введенных строк нахожу строку с макс уникальных символов
+            if (maxStr < count) {
+                maxStr = count;
+            }
+            System.out.println(phrases); // выводим строку с макс длиной уникальных символов
         }
-    }
+    */
 
+//Вариант 2
+        /*
+        Scanner in = new Scanner(System.in); // ввожу кол-во строк
+        System.out.println("Введите количество строк: ");
+        int n = in.nextInt();
+        String result = ""; // объявил пустую строку
+        String phrases; // объявляю переменную для ввода строк
+        for (int i = 1; i <= n; i++) { // запускаю цикл очередности ввода строк
+            System.out.println("Введите строку " + i + ": ");
+            phrases = in.next();
+            //in.next() вводит только одно слово до пробела
+            //если поменять на in.nextLine(), чтобы вводить всю строку целиком,
+            //то программа завершится после выбора кол-ва строк
+            char array[] = phrases.toCharArray();// объявляю массив и добавляю в его элементы символы введенной
+            for (char character : phrases.toCharArray()) { // считает уникальные символы *
+                if (!result.contains(String.valueOf(character))) result += character;
+            }
+        } System.out.print(result);
+        // * имеется пустая строка result и входная строка из n символов input_str
+        // по методу:
+        // Для ВСЕХ СИМВОЛОВ character ИЗ МАССИВА str.toCharArray() ВЫПОЛНИТЬ {
+        // ЕСЛИ (строка не содержит символ character)
+        // ТО к result добавить символ character;
+        // }
+         */
+    }
+}
