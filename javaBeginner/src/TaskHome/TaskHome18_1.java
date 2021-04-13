9847b2d790d5183f7734a58a00583e8bfca5ca34
DevOpsAT_Admin <F5-g6-h7>
2021-04-13 08:47:59 +0300

    решение в ИДЗ4

--- javaBeginner/src/TaskHome/TaskHome18_1.java
+++ javaBeginner/src/TaskHome/TaskHome18_1.java
@@ -1,48 +1,60 @@
-package TaskHome;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 import java.util.Scanner;
+
 public class TaskHome18_1 {
     public static void main(String[] args) {
         System.out.println("Отгадайте загадку: \nСидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает" + "\nУ вас есть три попытки!");
         String correctAnswer = "Заархивированный вирус";
         Scanner in = new Scanner(System.in);
         int attempt = 1;
+        boolean prompt = false;
         while (attempt <= 3) {
-            if (attempt < attempt + 1) {
-                System.out.println("Попытка " + attempt + ":");
-               // if (attempt == 1) { } // подсказка только на 1 шаге
+            System.out.println("Попытка " + attempt + ":");
 
+            if (attempt == 1) {
                 System.out.println("\nНужна подсказка?" +
-                            "\n- Да" + "\n- Нет");
-                    String promptInput = in.nextLine();
-                    String promptYes = "Да";
-                    boolean prompt = promptYes.equalsIgnoreCase(promptInput);
-                    if (prompt == true) {
-                        System.out.println("Подсказка: " + "Его побеждает прививка от Сетевого Доктора!" +
-                                "\nВведите ответ:");
-                        String Answer = in.nextLine();
-                        boolean result = correctAnswer.equalsIgnoreCase(Answer);
-
-                        if (result == true) {
-                            System.out.println("Правильно!");
-                        } else {
-                            System.out.println("Обидно, приходи в другой раз!:(");
-                        }
-                        break;
-                    } else {
-                        System.out.println("Первая попытка \nВведите ответ:");
-                        String Answer = in.nextLine();
-                        boolean result = correctAnswer.equalsIgnoreCase(Answer);
-                        if (result == true) {
-                            System.out.println("Правильно!");
-                            break;
-                        } else {
-                            System.out.println("Не правильно!");
-                        }
-                    }
-              //  }
-                attempt++;
+                        "\n- Да" + "\n- Нет");
+                String promptInput = in.nextLine();
+                String promptYes = "Да";
+                prompt = promptYes.equalsIgnoreCase(promptInput);
             }
-        } System.out.println("Конец");
+            if (prompt) {
+                System.out.println("Подсказка: " + "Его побеждает прививка от Сетевого Доктора!");
+                attempt = 3;
+                prompt = false;
+            }
+            System.out.println("Введите ответ:");
+            String Answer = in.nextLine();
+            boolean result = correctAnswer.equalsIgnoreCase(Answer);
+
+            if (result) {
+                System.out.println("Правильно!");
+                break;
+            } else {
+                attempt++;
+                System.out.println("Неправильно");
+                if (attempt > 3) {
+                    System.out.println("Обидно, приходи в другой раз!:(");
+                    break;
+                }
+            }
+        }
+//        else{
+//            System.out.println("Первая попытка \nВведите ответ:");
+//            String Answer = in.nextLine();
+//            boolean result = correctAnswer.equalsIgnoreCase(Answer);
+//            if (result == true) {
+//                System.out.println("Правильно!");
+//                break;
+//            } else {
+//                System.out.println("Не правильно!");
+//            }
+//        }
+        //  }
+        System.out.println("Конец");
     }
+
 }
+
